71b66dd2ffc4bc416e859eca22c46c3d5bac1104
Drull <pcaetanop@gmail.com>
2021-02-24 14:34:35 -0300

    Adding context api

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
-import styles from '../styles/components/CompletedChallenges.module.css'
+import styles from "../styles/components/CompletedChallenges.module.css";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import { useContext } from "react";
 
-export function CompletedChallenges(){
-    return(
-        <div className={styles.completedChallengesContainer}>
-            <span>Desafios Completos</span>
-            <span>5</span>
-        </div>
-    )
+export function CompletedChallenges() {
+  const { challengesCompleted } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.completedChallengesContainer}>
+      <span>Desafios Completos</span>
+      <span>{challengesCompleted}</span>
+    </div>
+  );
 }
